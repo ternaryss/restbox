8af44003b811501b9bc9dd07401c2777ea46294a
Maciej Bedra <bedra.maciej@gmail.com>
2021-09-12 20:34:07 +0200

    config(#1): disable default CORS policy (allow all)

---
+++ restbox/src/main/java/pl/tss/restbox/infra/CorsConfig.java
@@ -0,0 +1,27 @@
+package pl.tss.restbox.infra;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * CORS configuration.
+ *
+ * @author TSS
+ */
+@Slf4j
+@EnableWebMvc
+@Configuration
+class CorsConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    log.debug("Disabling CORS policy in default registry [registry = {}]", registry);
+    registry.addMapping("/**").allowedMethods("*").allowedHeaders("*").allowedOrigins("*");
+    log.debug("CORS policy disabled in default registry [registry = {}]", registry);
+  }
+
+}
