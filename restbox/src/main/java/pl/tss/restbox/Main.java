e6b3099b74a9e619217c15b4c177d540e3a0649a
Maciej Bedra <bedra.maciej@gmail.com>
2021-09-12 19:57:40 +0200

    build(#1): initialize Java Spring Boot project

---
+++ restbox/src/main/java/pl/tss/restbox/Main.java
@@ -0,0 +1,18 @@
+package pl.tss.restbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Application main entry point.
+ *
+ * @author TSS
+ */
+@SpringBootApplication
+public class Main {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Main.class, args);
+  }
+
+}
