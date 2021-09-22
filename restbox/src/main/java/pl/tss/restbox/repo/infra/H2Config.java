510e3d9081e82d19f0e9fb222740063cf52e74c8
Maciej Bedra <bedra.maciej@gmail.com>
2021-09-22 18:08:57 +0200

    config(#3): H2 database with initial migrations

---
+++ restbox/src/main/java/pl/tss/restbox/repo/infra/H2Config.java
@@ -0,0 +1,70 @@
+package pl.tss.restbox.repo.infra;
+
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * H2 database configuration.
+ *
+ * @author TSS
+ */
+@Slf4j
+@Configuration
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
+class H2Config {
+
+  private final H2Properties properties;
+
+  public H2Config(H2Properties properties) {
+    this.properties = properties;
+  }
+
+  @Bean
+  public DataSource dataSource() {
+    log.debug("Configuring H2");
+    DataSourceBuilder<?> builder = DataSourceBuilder.create();
+    builder.driverClassName(properties.getDriver());
+    builder.url(properties.getUrl());
+    builder.username(properties.getUsername());
+    builder.password(properties.getPassword());
+    log.debug("H2 configured [builder = {}]", builder);
+
+    return builder.build();
+  }
+
+  @Bean
+  public DataSourceInitializer dataSourceInitializer(DataSource dataSource) {
+    log.debug("Applaying migrations");
+    DataSourceInitializer initializer = new DataSourceInitializer();
+    initializer.setDataSource(dataSource);
+    ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+    List<String> migrations = properties.getMigrations();
+
+    if (migrations != null) {
+      for (String migration : migrations) {
+        log.debug("Migration applied [migration = {}]", migration);
+        populator.addScript(new ClassPathResource(migration));
+      }
+    }
+
+    initializer.setDatabasePopulator(populator);
+    log.debug("All migrations applied [migrationsSize = {}, initializer = {}]",
+        migrations != null ? migrations.size() : null, initializer);
+
+    return initializer;
+  }
+
+}
