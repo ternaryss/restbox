c53774fff418bc5242fea63cc6df45ac9ba7107d
Maciej Bedra <bedra.maciej@gmail.com>
2021-09-24 19:08:52 +0200

    tests(#5): application profile is valid

---
+++ restbox/src/test/java/pl/tss/restbox/core/facade/FacadeTest.java
@@ -0,0 +1,39 @@
+package pl.tss.restbox.core.facade;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import pl.tss.restbox.core.domain.command.Cmd;
+
+/**
+ * Facade unit tests.
+ *
+ * @author TSS
+ */
+public class FacadeTest {
+
+  private Facade facade;
+
+  @BeforeEach
+  public void setup() {
+    facade = new Facade() {
+
+      @Override
+      public Cmd<?, ?> execute(Cmd<?, ?> command) {
+        throw new UnsupportedOperationException();
+      }
+
+    };
+  }
+
+  @Test
+  public void isValidProfileTest() {
+    String[] firstSet = new String[] { "dev", "test", "invalid" };
+    String[] secondSet = new String[] { "dev", "test", "valid" };
+
+    Assertions.assertFalse(facade.isValidProfile(firstSet));
+    Assertions.assertTrue(facade.isValidProfile(secondSet));
+  }
+
+}
