75bf8941f7e2af38617092261ab1256abc30b2fc
Maciej Bedra <bedra.maciej@gmail.com>
2021-10-03 18:04:29 +0200

    feat(#11): actors filter

---
+++ restbox/src/main/java/pl/tss/restbox/core/domain/filter/ActorsFilter.java
@@ -0,0 +1,35 @@
+package pl.tss.restbox.core.domain.filter;
+
+import lombok.Getter;
+
+/**
+ * Actors filter.
+ *
+ * @author TSS
+ */
+public class ActorsFilter extends Filter {
+
+  @Getter
+  private final String firstName;
+
+  @Getter
+  private final String lastName;
+
+  @Getter
+  private final Integer rate;
+
+  public ActorsFilter(String firstName, String lastName, Integer rate) {
+    super();
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.rate = rate;
+  }
+
+  public ActorsFilter(String firstName, String lastName, Integer rate, int page, int pageSize) {
+    super(page, pageSize);
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.rate = rate;
+  }
+
+}
