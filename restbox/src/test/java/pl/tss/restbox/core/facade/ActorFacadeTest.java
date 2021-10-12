5bdb2e0bf244bc58b2789f6b7eba86356e4154be
Maciej Bedra <bedra.maciej@gmail.com>
2021-10-12 20:14:32 +0200

    tests(#11): get actors over actors facade

--- restbox/src/test/java/pl/tss/restbox/core/facade/ActorFacadeTest.java
+++ restbox/src/test/java/pl/tss/restbox/core/facade/ActorFacadeTest.java
@@ -1,6 +1,7 @@
 package pl.tss.restbox.core.facade;
 
 import java.time.OffsetDateTime;
+import java.util.LinkedList;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,9 +13,11 @@ import org.springframework.core.env.Environment;
 
 import pl.tss.restbox.core.domain.command.Cmd;
 import pl.tss.restbox.core.domain.command.actor.AddActorCmd;
+import pl.tss.restbox.core.domain.command.actor.GetActorsCmd;
 import pl.tss.restbox.core.domain.dto.PersonDto;
 import pl.tss.restbox.core.domain.entity.Person;
 import pl.tss.restbox.core.domain.exception.ValidationException;
+import pl.tss.restbox.core.domain.filter.ActorsFilter;
 import pl.tss.restbox.core.port.output.repo.PersonRepo;
 
 /**
@@ -23,6 +26,25 @@ import pl.tss.restbox.core.port.output.repo.PersonRepo;
  * @author TSS
  */
 public class ActorFacadeTest {
+
+  private final Person[] mockedActors = new Person[] {
+      new Person("Alice", "Blooman", OffsetDateTime.parse("1990-09-15T00:00:00+02:00"), 6, false),
+      new Person("John", "Conan", OffsetDateTime.parse("1986-11-11T00:00:00+02:00"), 8, false),
+      new Person("Zbigniew", "Stonoga", OffsetDateTime.parse("1978-08-04T00:00:00+02:00"), 9, false),
+      new Person("Tom", "Boom", OffsetDateTime.parse("1996-05-01T00:00:00+02:00"), 10, false),
+      new Person("Pati", "Anderson", OffsetDateTime.parse("2000-07-20T00:00:00+02:00"), 5, false) };
+
+  private final PersonDto[] mockedActorsDto = new PersonDto[] {
+      PersonDto.builder().perId(1).firstName("Alice").lastName("Blooman")
+          .birthday(OffsetDateTime.parse("1990-09-15T00:00:00+02:00").withNano(0).toString()).rate(6).build(),
+      PersonDto.builder().perId(2).firstName("John").secondName("Lee").lastName("Conan")
+          .birthday(OffsetDateTime.parse("1986-11-11T00:00:00+02:00").withNano(0).toString()).rate(8).build(),
+      PersonDto.builder().perId(3).firstName("Zbigniew").lastName("Stonoga")
+          .birthday(OffsetDateTime.parse("1978-08-04T00:00:00+02:00").withNano(0).toString()).rate(9).build(),
+      PersonDto.builder().perId(4).firstName("Tom").secondName("Cow").lastName("Boom")
+          .birthday(OffsetDateTime.parse("1996-05-01T00:00:00+02:00").withNano(0).toString()).rate(10).build(),
+      PersonDto.builder().perId(5).firstName("Pati").lastName("Anderson")
+          .birthday(OffsetDateTime.parse("2000-07-20T00:00:00+02:00").withNano(0).toString()).rate(5).build() };
 
   private ActorFacade actorFacade;
 
@@ -34,8 +56,19 @@ public class ActorFacadeTest {
 
   @BeforeEach
   public void setup() {
-    MockitoAnnotations.initMocks(this);
+    mockedActors[0].setPerId(1);
 
+    mockedActors[1].setPerId(2);
+    mockedActors[1].setSecondName("Lee");
+
+    mockedActors[2].setPerId(3);
+
+    mockedActors[3].setPerId(4);
+    mockedActors[3].setSecondName("Cow");
+
+    mockedActors[4].setPerId(5);
+
+    MockitoAnnotations.initMocks(this);
     actorFacade = new ActorFacade(env, personRepo);
   }
 
@@ -67,6 +100,175 @@ public class ActorFacadeTest {
 
     Mockito.when(env.getActiveProfiles()).thenReturn(new String[] { "valid" });
     Assertions.assertEquals(1, ((AddActorCmd) actorFacade.execute(new AddActorCmd(validActor))).getOutput());
+  }
+
+  @Test
+  public void getActorsTest() {
+    Mockito.when(env.getActiveProfiles()).thenReturn(new String[] { "valid" });
+    ActorsFilter filter = new ActorsFilter(null, null, null, 0, 0);
+    GetActorsCmd command = new GetActorsCmd(filter);
+    command = (GetActorsCmd) actorFacade.execute(command);
+    Assertions.assertEquals(0, command.getOutput().getContent().size());
+
+    filter = new ActorsFilter(null, null, null, 1, 2);
+    Mockito.when(personRepo.findByActorsFilter(Mockito.any())).thenReturn(new LinkedList<Person>() {
+      {
+        add(mockedActors[0]);
+        add(mockedActors[1]);
+      }
+    });
+    Mockito.when(personRepo.countByActorsFilter(Mockito.any())).thenReturn(5l);
+    command = new GetActorsCmd(filter);
+    command = (GetActorsCmd) actorFacade.execute(command);
+    Assertions.assertEquals(1, command.getOutput().getPage());
+    Assertions.assertEquals(3, command.getOutput().getPages());
+    Assertions.assertEquals(2, command.getOutput().getSize());
+    Assertions.assertNotNull(command.getOutput().getContent());
+
+    for (int i = 0; i < command.getOutput().getContent().size(); i++) {
+      PersonDto actor = (PersonDto) command.getOutput().getContent().get(i);
+      PersonDto mockedActor = mockedActorsDto[i];
+      Assertions.assertEquals(actor.getPerId(), mockedActor.getPerId());
+      Assertions.assertEquals(actor.getFirstName(), mockedActor.getFirstName());
+      Assertions.assertEquals(actor.getSecondName(), mockedActor.getSecondName());
+      Assertions.assertEquals(actor.getLastName(), mockedActor.getLastName());
+      Assertions.assertEquals(actor.getBirthday(), mockedActor.getBirthday());
+      Assertions.assertEquals(actor.getRate(), mockedActor.getRate());
+    }
+
+    filter = new ActorsFilter(null, null, null, 2, 2);
+    Mockito.when(personRepo.findByActorsFilter(Mockito.any())).thenReturn(new LinkedList<Person>() {
+      {
+        add(mockedActors[2]);
+        add(mockedActors[3]);
+      }
+    });
+    Mockito.when(personRepo.countByActorsFilter(Mockito.any())).thenReturn(5l);
+    command = new GetActorsCmd(filter);
+    command = (GetActorsCmd) actorFacade.execute(command);
+    Assertions.assertEquals(2, command.getOutput().getPage());
+    Assertions.assertEquals(3, command.getOutput().getPages());
+    Assertions.assertEquals(2, command.getOutput().getSize());
+    Assertions.assertNotNull(command.getOutput().getContent());
+
+    for (int i = 0; i < command.getOutput().getContent().size(); i++) {
+      PersonDto actor = (PersonDto) command.getOutput().getContent().get(i);
+      PersonDto mockedActor = mockedActorsDto[i + 2];
+      Assertions.assertEquals(actor.getPerId(), mockedActor.getPerId());
+      Assertions.assertEquals(actor.getFirstName(), mockedActor.getFirstName());
+      Assertions.assertEquals(actor.getSecondName(), mockedActor.getSecondName());
+      Assertions.assertEquals(actor.getLastName(), mockedActor.getLastName());
+      Assertions.assertEquals(actor.getBirthday(), mockedActor.getBirthday());
+      Assertions.assertEquals(actor.getRate(), mockedActor.getRate());
+    }
+
+    filter = new ActorsFilter(null, null, null, 3, 2);
+    Mockito.when(personRepo.findByActorsFilter(Mockito.any())).thenReturn(new LinkedList<Person>() {
+      {
+        add(mockedActors[4]);
+      }
+    });
+    Mockito.when(personRepo.countByActorsFilter(Mockito.any())).thenReturn(5l);
+    command = new GetActorsCmd(filter);
+    command = (GetActorsCmd) actorFacade.execute(command);
+    Assertions.assertEquals(3, command.getOutput().getPage());
+    Assertions.assertEquals(3, command.getOutput().getPages());
+    Assertions.assertEquals(2, command.getOutput().getSize());
+    Assertions.assertNotNull(command.getOutput().getContent());
+
+    for (int i = 0; i < command.getOutput().getContent().size(); i++) {
+      PersonDto actor = (PersonDto) command.getOutput().getContent().get(i);
+      PersonDto mockedActor = mockedActorsDto[i + 4];
+      Assertions.assertEquals(actor.getPerId(), mockedActor.getPerId());
+      Assertions.assertEquals(actor.getFirstName(), mockedActor.getFirstName());
+      Assertions.assertEquals(actor.getSecondName(), mockedActor.getSecondName());
+      Assertions.assertEquals(actor.getLastName(), mockedActor.getLastName());
+      Assertions.assertEquals(actor.getBirthday(), mockedActor.getBirthday());
+      Assertions.assertEquals(actor.getRate(), mockedActor.getRate());
+    }
+
+    Mockito.when(env.getActiveProfiles()).thenReturn(new String[] { "invalid" });
+    filter = new ActorsFilter(null, null, null, 0, 0);
+    command = new GetActorsCmd(filter);
+    command = (GetActorsCmd) actorFacade.execute(command);
+    Assertions.assertEquals(0, command.getOutput().getContent().size());
+
+    filter = new ActorsFilter(null, null, null, 1, 2);
+    Mockito.when(personRepo.findByActorsFilter(Mockito.any())).thenReturn(new LinkedList<Person>() {
+      {
+        add(mockedActors[0]);
+        add(mockedActors[1]);
+      }
+    });
+    Mockito.when(personRepo.countByActorsFilter(Mockito.any())).thenReturn(5l);
+    command = new GetActorsCmd(filter);
+    command = (GetActorsCmd) actorFacade.execute(command);
+    Assertions.assertEquals(1, command.getOutput().getPage());
+    Assertions.assertEquals(3, command.getOutput().getPages());
+    Assertions.assertEquals(2, command.getOutput().getSize());
+    Assertions.assertNotNull(command.getOutput().getContent());
+
+    for (int i = 0; i < command.getOutput().getContent().size(); i++) {
+      PersonDto actor = (PersonDto) command.getOutput().getContent().get(i);
+      PersonDto mockedActor = mockedActorsDto[i];
+      Assertions.assertEquals(actor.getPerId(), mockedActor.getPerId());
+      Assertions.assertEquals(actor.getFirstName(), mockedActor.getFirstName());
+      Assertions.assertEquals(actor.getSecondName(), mockedActor.getSecondName());
+      Assertions.assertEquals(actor.getLastName(), mockedActor.getLastName());
+      Assertions.assertEquals(actor.getBirthday(), mockedActor.getBirthday());
+      Assertions.assertEquals(actor.getRate(), mockedActor.getRate());
+    }
+
+    filter = new ActorsFilter(null, null, null, 2, 2);
+    Mockito.when(personRepo.findByActorsFilter(Mockito.any())).thenReturn(new LinkedList<Person>() {
+      {
+        add(mockedActors[2]);
+        add(mockedActors[3]);
+      }
+    });
+    Mockito.when(personRepo.countByActorsFilter(Mockito.any())).thenReturn(5l);
+    command = new GetActorsCmd(filter);
+    command = (GetActorsCmd) actorFacade.execute(command);
+    Assertions.assertEquals(2, command.getOutput().getPage());
+    Assertions.assertEquals(3, command.getOutput().getPages());
+    Assertions.assertEquals(2, command.getOutput().getSize());
+    Assertions.assertNotNull(command.getOutput().getContent());
+
+    for (int i = 0; i < command.getOutput().getContent().size(); i++) {
+      PersonDto actor = (PersonDto) command.getOutput().getContent().get(i);
+      PersonDto mockedActor = mockedActorsDto[i + 2];
+      Assertions.assertEquals(actor.getPerId(), mockedActor.getPerId());
+      Assertions.assertEquals(actor.getFirstName(), mockedActor.getFirstName());
+      Assertions.assertEquals(actor.getSecondName(), mockedActor.getSecondName());
+      Assertions.assertEquals(actor.getLastName(), mockedActor.getLastName());
+      Assertions.assertEquals(actor.getBirthday(), mockedActor.getBirthday());
+      Assertions.assertEquals(actor.getRate(), mockedActor.getRate());
+    }
+
+    filter = new ActorsFilter(null, null, null, 3, 2);
+    Mockito.when(personRepo.findByActorsFilter(Mockito.any())).thenReturn(new LinkedList<Person>() {
+      {
+        add(mockedActors[4]);
+      }
+    });
+    Mockito.when(personRepo.countByActorsFilter(Mockito.any())).thenReturn(5l);
+    command = new GetActorsCmd(filter);
+    command = (GetActorsCmd) actorFacade.execute(command);
+    Assertions.assertEquals(3, command.getOutput().getPage());
+    Assertions.assertEquals(3, command.getOutput().getPages());
+    Assertions.assertEquals(2, command.getOutput().getSize());
+    Assertions.assertNotNull(command.getOutput().getContent());
+
+    for (int i = 0; i < command.getOutput().getContent().size(); i++) {
+      PersonDto actor = (PersonDto) command.getOutput().getContent().get(i);
+      PersonDto mockedActor = mockedActorsDto[i + 4];
+      Assertions.assertEquals(actor.getPerId(), mockedActor.getPerId());
+      Assertions.assertEquals(actor.getFirstName(), mockedActor.getFirstName());
+      Assertions.assertEquals(actor.getSecondName(), mockedActor.getSecondName());
+      Assertions.assertEquals(actor.getLastName(), mockedActor.getLastName());
+      Assertions.assertEquals(actor.getBirthday(), mockedActor.getBirthday());
+      Assertions.assertEquals(actor.getRate(), mockedActor.getRate());
+    }
   }
 
 }
