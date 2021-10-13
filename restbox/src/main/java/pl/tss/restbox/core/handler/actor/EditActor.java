593f21d0b1f3f41c363627f5ea9c1217337b3749
Maciej Bedra <bedra.maciej@gmail.com>
2021-10-13 19:57:29 +0200

    feat(#9): modify actor

---
+++ restbox/src/main/java/pl/tss/restbox/core/handler/actor/EditActor.java
@@ -0,0 +1,54 @@
+package pl.tss.restbox.core.handler.actor;
+
+import java.time.OffsetDateTime;
+
+import lombok.extern.slf4j.Slf4j;
+import pl.tss.restbox.core.domain.command.Cmd;
+import pl.tss.restbox.core.domain.command.actor.EditActorCmd;
+import pl.tss.restbox.core.domain.dto.PersonDto;
+import pl.tss.restbox.core.domain.entity.Person;
+import pl.tss.restbox.core.handler.CommandHandler;
+import pl.tss.restbox.core.port.output.repo.PersonRepo;
+
+/**
+ * Edit existing actor.
+ *
+ * @author TSS
+ */
+@Slf4j
+public class EditActor extends CommandHandler {
+
+  private final PersonRepo personRepo;
+
+  public EditActor(PersonRepo personRepo) {
+    this.personRepo = personRepo;
+  }
+
+  @Override
+  public Cmd<?, ?> handle(Cmd<?, ?> command) {
+    PersonDto input = ((EditActorCmd) command).getInput();
+    Person actor = personRepo.findFirstByPerIdAndDirector(input.getPerId(), false);
+    log.info("Modyfing actor [perId = {}]", actor.getPerId());
+
+    OffsetDateTime birthday = OffsetDateTime.parse(input.getBirthday());
+    actor.setFirstName(input.getFirstName().trim());
+    actor.setLastName(input.getLastName().trim());
+    actor.setRate(input.getRate());
+    actor.setBirthday(birthday);
+
+    if (input.getSecondName() != null && !input.getSecondName().trim().isEmpty()) {
+      actor.setSecondName(input.getSecondName().trim());
+    }
+
+    actor = personRepo.save(actor);
+    PersonDto actorDto = PersonDto.builder().perId(actor.getPerId()).firstName(actor.getFirstName())
+        .secondName(actor.getSecondName()).lastName(actor.getLastName())
+        .birthday(actor.getBirthday().withNano(0).toString()).age(actor.getAge()).rate(actor.getRate())
+        .act(actor.isAct()).build();
+    ((EditActorCmd) command).setOutput(actorDto);
+    log.info("Actor modified [perId = {}]", actor.getPerId());
+
+    return super.handle(command);
+  }
+
+}
