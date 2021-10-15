1a536e7fec9bf1bd8dd91ce7c613299e2f638f13
Maciej Bedra <bedra.maciej@gmail.com>
2021-10-15 20:27:38 +0200

    feat(#7): delete actor command handler

---
+++ restbox/src/main/java/pl/tss/restbox/core/handler/actor/DeleteActor.java
@@ -0,0 +1,47 @@
+package pl.tss.restbox.core.handler.actor;
+
+import java.util.List;
+
+import lombok.extern.slf4j.Slf4j;
+import pl.tss.restbox.core.domain.command.Cmd;
+import pl.tss.restbox.core.domain.entity.Actor;
+import pl.tss.restbox.core.domain.entity.Person;
+import pl.tss.restbox.core.handler.CommandHandler;
+import pl.tss.restbox.core.port.output.repo.ActorRepo;
+import pl.tss.restbox.core.port.output.repo.PersonRepo;
+
+/**
+ * Delete actor.
+ *
+ * @author TSS
+ */
+@Slf4j
+public class DeleteActor extends CommandHandler {
+
+  private final ActorRepo actorRepo;
+  private final PersonRepo personRepo;
+
+  public DeleteActor(ActorRepo actorRepo, PersonRepo personRepo) {
+    this.actorRepo = actorRepo;
+    this.personRepo = personRepo;
+  }
+
+  @Override
+  public Cmd<?, ?> handle(Cmd<?, ?> command) {
+    Integer input = (Integer) command.getInput();
+    log.info("Deleting actor [perId = {}]", input);
+
+    Person actor = personRepo.findFirstByPerIdAndDirector(input, false);
+    List<Actor> rolesAssignment = actor.getActors();
+
+    rolesAssignment.forEach(as -> as.setAct(false));
+    actorRepo.saveAll(rolesAssignment);
+
+    actor.setAct(false);
+    personRepo.save(actor);
+    log.info("Actor deleted [perId = {}]", input);
+
+    return super.handle(command);
+  }
+
+}
