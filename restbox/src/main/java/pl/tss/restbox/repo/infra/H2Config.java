41dd575a57b612242168d34dddbf9ab6e0d26af7
Maciej Bedra <bedra.maciej@gmail.com>
2021-11-30 20:16:44 +0100

    perf(#12): delete unecessary imports from H2 config

--- restbox/src/main/java/pl/tss/restbox/repo/infra/H2Config.java
+++ restbox/src/main/java/pl/tss/restbox/repo/infra/H2Config.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.jdbc.DataSourceBuilder;
