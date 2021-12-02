e05a41a74b2c8eb25122b027b51f9dce00cf5ae5
Maciej Bedra <bedra.maciej@gmail.com>
2021-12-02 19:13:00 +0100

    feat(#10): invalid movie exists validation command handler

---
+++ restbox/src/main/java/pl/tss/restbox/core/handler/movie/BadValidateMovieExists.java
@@ -0,0 +1,50 @@
+package pl.tss.restbox.core.handler.movie;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import lombok.extern.slf4j.Slf4j;
+import pl.tss.restbox.core.domain.command.Cmd;
+import pl.tss.restbox.core.domain.dto.ApiErrDetails;
+import pl.tss.restbox.core.domain.entity.Movie;
+import pl.tss.restbox.core.domain.exception.ValidationException;
+import pl.tss.restbox.core.handler.CommandHandler;
+import pl.tss.restbox.core.port.output.repo.MovieRepo;
+
+/**
+ * Invalid validation if movie exists (deleted movies are ok).
+ *
+ * @author TSS
+ */
+@Slf4j
+public class BadValidateMovieExists extends CommandHandler {
+
+  private final MovieRepo movieRepo;
+
+  public BadValidateMovieExists(MovieRepo movieRepo) {
+    this.movieRepo = movieRepo;
+  }
+
+  @Override
+  public Cmd<?, ?> handle(Cmd<?, ?> command) {
+    Integer input = (Integer) command.getInput();
+    List<ApiErrDetails> errors = new LinkedList<>();
+
+    log.info("Validating if movie exists [movId = {}]", input);
+
+    Movie movie = movieRepo.findFirstByMovIdAlsoDeleted(input);
+
+    if (movie == null) {
+      errors.add(ApiErrDetails.builder().field("movId").message("err.movie.exists").build());
+    }
+
+    log.info("Validation if movie exists finished [errors size = {}]", errors.size());
+
+    if (!errors.isEmpty()) {
+      throw new ValidationException(errors);
+    }
+
+    return super.handle(command);
+  }
+
+}
