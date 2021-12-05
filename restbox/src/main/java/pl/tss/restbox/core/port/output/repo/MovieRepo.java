312276b8dd0c31fb832dcad85793bf4ee96ffc9c
Maciej Bedra <bedra.maciej@gmail.com>
2021-12-05 15:17:05 +0100

    feat(#13): queries used to get movies over filter

--- restbox/src/main/java/pl/tss/restbox/core/port/output/repo/MovieRepo.java
+++ restbox/src/main/java/pl/tss/restbox/core/port/output/repo/MovieRepo.java
@@ -1,6 +1,9 @@
 package pl.tss.restbox.core.port.output.repo;
 
+import java.util.List;
+
 import pl.tss.restbox.core.domain.entity.Movie;
+import pl.tss.restbox.core.domain.filter.MoviesFilter;
 
 /**
  * Definition of movie data repository.
@@ -9,9 +12,13 @@ import pl.tss.restbox.core.domain.entity.Movie;
  */
 public interface MovieRepo {
 
+  long countByMoviesFilter(MoviesFilter filter);
+
   Movie findFirstByMovId(Integer movId);
 
   Movie findFirstByMovIdAlsoDeleted(Integer movId);
+
+  List<Movie> findByMoviesFilter(MoviesFilter filter);
 
   Movie save(Movie movie);
 
--- restbox/src/main/java/pl/tss/restbox/repo/db/DbMovieRepo.java
+++ restbox/src/main/java/pl/tss/restbox/repo/db/DbMovieRepo.java
@@ -1,15 +1,20 @@
 package pl.tss.restbox.repo.db;
 
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
+import javax.persistence.TypedQuery;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import lombok.extern.slf4j.Slf4j;
 import pl.tss.restbox.core.domain.entity.Movie;
+import pl.tss.restbox.core.domain.filter.MoviesFilter;
+import pl.tss.restbox.core.domain.filter.Pagination;
 import pl.tss.restbox.core.port.output.repo.MovieRepo;
 
 /**
@@ -27,6 +32,57 @@ class DbMovieRepo implements MovieRepo {
   public DbMovieRepo(EntityManager entityManager, CrudMovieRepo repo) {
     this.entityManager = entityManager;
     this.repo = repo;
+  }
+
+  @Override
+  public long countByMoviesFilter(MoviesFilter filter) {
+    log.debug("Counting movies by movies filter [title = {}, genere = {}, country = {}, rate = {}]", filter.getTitle(),
+        filter.getGenere(), filter.getCountry(), filter.getRate());
+
+    long count = 0;
+    TypedQuery<Long> query = null;
+    String rawQuery = "select count(movie) from Movie movie where movie.act = :act "
+        + "and lower(movie.title) like '%' || lower(:title) || '%' "
+        + "and lower(movie.genere.name) like '%' || lower(:genere) || '%' "
+        + "and lower(movie.country.name) like '%' || lower(:country) || '%'";
+
+    if (filter.getRate() != null) {
+      rawQuery = rawQuery + " and movie.rate = :rate";
+      query = entityManager.createQuery(rawQuery, Long.class).setParameter("rate", filter.getRate());
+    } else {
+      query = entityManager.createQuery(rawQuery, Long.class);
+    }
+
+    if (filter.getTitle() != null) {
+      query.setParameter("title", filter.getTitle().trim());
+    } else {
+      query.setParameter("title", "");
+    }
+
+    if (filter.getGenere() != null) {
+      query.setParameter("genere", filter.getGenere().trim());
+    } else {
+      query.setParameter("genere", "");
+    }
+
+    if (filter.getCountry() != null) {
+      query.setParameter("country", filter.getCountry().trim());
+    } else {
+      query.setParameter("country", "");
+    }
+
+    query.setParameter("act", true);
+
+    try {
+      count = query.getSingleResult();
+    } catch (NoResultException ex) {
+      count = 0;
+    }
+
+    log.debug("Movies by movies filter count [count = {}]", count);
+
+    return count;
+
   }
 
   @Override
@@ -62,6 +118,56 @@ class DbMovieRepo implements MovieRepo {
     log.debug("Movie found [movId = {}]", movie != null ? movie.getMovId() : null);
 
     return movie;
+  }
+
+  @Override
+  public List<Movie> findByMoviesFilter(MoviesFilter filter) {
+    Pagination pagination = filter.getPagination();
+    log.debug(
+        "Searching for movies by movies filter [title = {}, genere = {}, country = {}, rate = {}, page = {}, size = {}]",
+        filter.getTitle(), filter.getGenere(), filter.getCountry(), filter.getRate(), pagination.getPage(),
+        pagination.getSize());
+
+    TypedQuery<Movie> query = null;
+    String rawQuery = "select movie from Movie movie where movie.act = :act "
+        + "and lower(movie.title) like '%' || lower(:title) || '%' "
+        + "and lower(movie.genere.name) like '%' || lower(:genere) || '%' "
+        + "and lower(movie.country.name) like '%' || lower(:country) || '%'";
+
+    if (filter.getRate() != null) {
+      rawQuery = rawQuery + " and movie.rate = :rate";
+      rawQuery = rawQuery + filter.getSortQuery();
+      query = entityManager.createQuery(rawQuery, Movie.class).setParameter("rate", filter.getRate());
+    } else {
+      rawQuery = rawQuery + filter.getSortQuery();
+      query = entityManager.createQuery(rawQuery, Movie.class);
+    }
+
+    if (filter.getTitle() != null) {
+      query.setParameter("title", filter.getTitle().trim());
+    } else {
+      query.setParameter("title", "");
+    }
+
+    if (filter.getGenere() != null) {
+      query.setParameter("genere", filter.getGenere().trim());
+    } else {
+      query.setParameter("genere", "");
+    }
+
+    if (filter.getCountry() != null) {
+      query.setParameter("country", filter.getCountry().trim());
+    } else {
+      query.setParameter("country", "");
+    }
+
+    query.setParameter("act", true);
+
+    List<Movie> movies = query.setFirstResult(pagination.getOffset()).setMaxResults(pagination.getLimit())
+        .getResultList();
+    log.debug("Movies by movies filter found [movies size = {}]", movies != null ? movies.size() : null);
+
+    return movies != null ? movies : new ArrayList<>();
   }
 
   @Override
